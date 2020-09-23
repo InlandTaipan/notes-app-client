76839e8bccc289edea3d7a6dc1c6fb4adfcb9af0
Maximo van der Raadt <mvanderr@edu.uwaterloo.ca>
2020-09-23 12:07:27 -0400

    Setting up react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-2",
+        BUCKET: "note-files-upload"
+    },
+    apiGateway: {
+        REGION: "us-east-2",
+        URL: "https://1wtkep8ktc.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-2",
+        USER_POOL_ID: "us-east-2_bGnuiz0i2",
+        APP_CLIENT_ID: "7gpo8di895k1r1jpqem5f2npmn",
+        IDENTITY_POOL_ID: "us-east-2:cf51a934-aada-4da7-974d-ee18fc6c4156"
+    }
+};
---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Home.css";
+
+export default function Home() {
+    return (
+        <div className="Home">
+            <div className="Lander">
+                <h1>Bangin'</h1>
+                <p>Bangin' notes for bangin' people</p>
+            </div>
+        </div>
+    );
+}
