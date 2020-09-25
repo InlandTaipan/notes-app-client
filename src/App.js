401ed6ff405a12b0fe86ec1eb5d51d182f162787
Maximo van der Raadt <mvanderr@edu.uwaterloo.ca>
2020-09-25 13:46:24 -0400

    Error handling

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { AppContext } from "./libs/contextLib";
 import { Auth } from "aws-amplify";
 import "./App.css";
 import { onError } from './libs/errorLib';
+import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
   const [isAuthenticated, userHasAuthenticated] = useState(false);
@@ -70,9 +71,11 @@ function App() {
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <AppContext.Provider value={{isAuthenticated, userHasAuthenticated}}>
-        <Routes />
-      </AppContext.Provider>
+      <ErrorBoundary>
+        <AppContext.Provider value={{isAuthenticated, userHasAuthenticated}}>
+          <Routes />
+        </AppContext.Provider>
+      </ErrorBoundary>
     </div>
   );
 }
--- src/libs/errorLib.js
+++ src/libs/errorLib.js
@@ -1,10 +1,46 @@
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+const isLocal = process.env.NODE_ENV === "development";
+
+export function initSentry() {
+    if (isLocal) {
+        return;
+    }
+
+    Sentry.init({
+        dsn: "https://7abf0fc70e5f4ab79cf73820e8887062@o453103.ingest.sentry.io/5441515",
+        integrations: [
+          new Integrations.BrowserTracing(),
+        ],
+        tracesSampleRate: 1.0,
+      });
+}
+
+export function logError(error, errorInfo = null) {
+    if (isLocal) {
+        return;
+    }
+
+    Sentry.withScope((scope => {
+        errorInfo && scope.setExtras(errorInfo);
+        Sentry.captureException(error);
+    }));
+}
+
 export function onError(error) {
+    let errorInfo = {};
     let message = error.toString();
 
     //Authentication errors
     if (!(error instanceof Error) && error.message) {
+        errorInfo = error;
         message = error.message;
+        error = new Error(message);
+    } else if (error.config && error.config.url) {
+        errorInfo.url = error.config.url;
     }
 
+    logError(error, errorInfo);
     alert(message);
 }
